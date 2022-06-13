02cfb5088d930416b60794785a5277a001fa236a
MilesAngels <mangels@alphaworks.tech>
2022-06-13 16:58:55 -0700

    front page

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'miles-marie-angels' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">button</button>
 			<?php
 			wp_nav_menu(
 				array(
--- template-parts/hero-section.php
+++ template-parts/hero-section.php
@@ -1,5 +1,5 @@
 <section class="hero-section">
-    <div class="hero-image" style="background-image: url('http://miles-marie-angels.local/wp-content/uploads/2022/06/1217172.jpg'); background-size: cover;">
+    <div class="hero-image" style="background-image: url('http://miles-marie-angels.local/wp-content/uploads/2022/06/1217172.jpg');">
     
         <div class="overlay">
             <p>Hey! My name is Miles.</p>
