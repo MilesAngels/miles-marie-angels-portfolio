f2d27b758fbaf984f95ea047c6a38c0bedeb1172
MilesAngels <mangels@alphaworks.tech>
2022-06-15 16:37:56 -0700

    gallery

--- template-parts/projects-gallery.php
+++ template-parts/projects-gallery.php
@@ -1,8 +1,8 @@
 <section class="gallery">
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
+    <div class="cards"><a href="https://milesangels.github.io/FinalProject/index.html"><img src="" alt="#"></a></div>
+    <div class="cards"><a href=""><img src="" alt="#"></a></div>
+    <div class="cards"><a href=""><img src="" alt="#"></a></div>
+    <div class="cards"><a href=""><img src="" alt="#"></a></div>
+    <div class="cards"><a href=""><img src="" alt="#"></a></div>
+    <div class="cards"><a href=""><img src="" alt="#"></a></div>
 </section>
