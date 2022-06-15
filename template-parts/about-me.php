6219cb5c036f0e199d2ad2adb963df38af3f03d5
MilesAngels <mangels@alphaworks.tech>
2022-06-15 09:31:44 -0700

    about-page skills and background

--- template-parts/about-me.php
+++ template-parts/about-me.php
@@ -5,7 +5,15 @@
     <div class="about-text">
         <h2>About Me</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <h3>My Projects &#x2192;</h3>
-        <h3>My Art &#x2192;</h3>
+        <h3><a href="#">My Projects &#x2192;</a></h3>
+        <h3><a href="#">My Art &#x2192;</a></h3>
+    </div>
+</section>
+<section class="credentials">
+    <div class="skills">
+        <h3>Skillset</h3>
+    </div>
+    <div class="background">
+        <h3>Background</h3>
     </div>
 </section>
