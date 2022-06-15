55bf2ffa15080ef621556806297ba2efac51c5c7
MilesAngels <mangels@alphaworks.tech>
2022-06-15 15:57:59 -0700

    gallery

---
+++ template-parts/contact-form.php
@@ -0,0 +1,29 @@
+<section class="form-overlay">
+    <form action="#">
+        <h1>Contact Me</h1>
+        <!-- Name -->
+        <div class="name">
+            <label for="name"></label>
+            <input type="text" placeholder="Name:">
+        </div>
+
+        <!-- Email -->
+        <div class="email">
+            <label for="email"></label>
+            <input type="email" placeholder="Email:">
+        </div>
+        
+        <!-- Subject -->
+        <div class="subject-line">
+            <label for="subject-line"></label>
+            <input type="text" placeholder="Subject Line">
+        </div>
+        
+        <!-- Message -->
+        <div class="message">
+            <label for="message"></label>
+            <textarea name="message" placeholder="Message"></textarea>
+        </div>
+    </form>
+    
+</section>
--- template-parts/hero-section.php
+++ template-parts/hero-section.php
@@ -8,10 +8,12 @@
             <p>I'm a WordPress developer.</p>
             <button class="project-button"><a href="#">See my work</a></button>
         </div>
-    <?php elseif (is_page() && !is_page('contact/')): ?>
+    <?php elseif (is_page() && !is_page('contact')): ?>
         <div class="overlay center">
             <h1><?php the_field('overlay_text'); ?></h1>
 
         </div>
+    <?php elseif (is_page('contact')):?>
+        <?php get_template_part('template-parts/contact-form');?>
     <?php endif; ?>
 </section>
