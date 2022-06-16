da69f4a725ee15e1fccaa128d562c2029e7f413c
MilesAngels <mangels@alphaworks.tech>
2022-06-16 09:06:42 -0700

    gallery card

--- template-parts/projects-gallery.php
+++ template-parts/projects-gallery.php
@@ -1,5 +1,13 @@
+<!-- My Website Gallery -->
 <section class="gallery">
-    <div class="cards"><a href="https://milesangels.github.io/FinalProject/index.html"><img src="<?php the_field('image_1'); ?>"></a></div>
+    <div class="cards" style="background:url('<?php the_field('image_1'); ?>') center center; background-size: cover; background-repeat: no-repeat;">
+        <div class="card-content">
+            <h2 class="card-title">Name of Website</h2>
+            <a href="https://milesangels.github.io/FinalProject/index.html">
+                <button class="redirect-button">Learn More!</button>
+            </a>
+        </div>
+    </div>
     <div class="cards"><a href=""><img src="" alt="#"></a></div>
     <div class="cards"><a href=""><img src="" alt="#"></a></div>
     <div class="cards"><a href=""><img src="" alt="#"></a></div>
