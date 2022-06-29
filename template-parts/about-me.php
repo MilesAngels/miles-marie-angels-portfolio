a7e0c708f0e59945ebd5d40417ba1f39c0785c3d
MilesAngels <mangels@alphaworks.tech>
2022-06-29 10:35:41 -0700

    projects page

--- template-parts/about-me.php
+++ template-parts/about-me.php
@@ -1,12 +1,11 @@
 <section class="about-me-container">
     <div class="about-image-container">
-        <div class="about-image">
-            <img src="http://miles-marie-angels.local/wp-content/uploads/2022/06/20220624_161631-scaled.jpg" height="300" >
-        </div>
+        <div class="about-image" style="background-image: url('http://miles-marie-angels.local/wp-content/uploads/2022/06/20220624_161631-scaled.jpg'); background-size: cover; height: 400px; background-repeat: no-repeat;"></div>
     </div>
     <div class="about-text">
         <h2 class="montserrat">Hi, I'm Miles Angels!</h2>
-        <p></p>
+        <p>I am aspiring Wordpress Developer apprentice who enjoys problem solving: be it in web developing or mathematics. Creating accessible websites has been a passion of mine and making sure everyone can have equal oportunity to enjoy the websites I create.</p>
+        <p>Playing vidoegames and creating art are some of my hobbies. I have been slowly discovering the wonders of digital art and am working towards being good at it.</p>
         <h3><a href="<?php echo get_permalink(get_page_by_path('projects'))?>">My Projects &#x2192;</a></h3>
         <h3><a href="<?php echo get_permalink(get_page_by_path('art'))?>">My Art &#x2192;</a></h3>
     </div>
