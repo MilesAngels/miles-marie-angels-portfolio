4f09d7376810a6a8cb453909c3a66930ac14d69e
MilesAngels <mangels@alphaworks.tech>
2022-07-08 14:20:30 -0700

    art gallery links

--- template-parts/art-gallery-cards.php
+++ template-parts/art-gallery-cards.php
@@ -37,6 +37,15 @@
         </div>
       </div>
     </div>
+    <div class="img-box">
+      <img src="https://miles-marie-angels.local/wp-content/uploads/2022/07/20220527_092657-scaled-e1657311259707.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Save Point</p>
+          <p class="opacity-low">Miles Angels</p>
+        </div>
+      </div>
+    </div>
   </div>
   
 
@@ -47,6 +56,37 @@
       <div class="transparent-box">
         <div class="caption">
           <p><a href="http://miles-marie-angels.local/cat-in-the-clouds/">Cat in the Clouds</a></p>
+          <p class="opacity-low">Miles Angels</p>
+        </div>
+      </div> 
+    </div>
+    <div class="img-box">
+      <img src="http://miles-marie-angels.local/wp-content/uploads/2022/06/Space-1.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Space</p>
+          <p class="opacity-low">Miles Angels</p>
+        </div>
+      </div>
+    </div>
+    <div class="img-box">
+      <img src="http://miles-marie-angels.local/wp-content/uploads/2022/06/Dream.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p>Adventure</p>
+          <p class="opacity-low">Miles Angels</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <h2 class="category montserrat">Images</h2>
+  <div class="gallery-image">
+    <div class="img-box">
+      <img src="https://miles-marie-angels.local/wp-content/uploads/2022/07/20220624_205841-scaled.jpg" alt="" />
+      <div class="transparent-box">
+        <div class="caption">
+          <p><a href="http://miles-marie-angels.local/cat-in-the-clouds/">River side</a></p>
           <p class="opacity-low">Miles Angels</p>
         </div>
       </div> 
