794e09c426a841673ab42d5e0430e271ad166ef8
MilesAngels <mangels@alphaworks.tech>
2022-07-18 13:03:36 -0700

    comments

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,8 @@
+const preloader = document.querySelector('.preloader');
+
 
 window.addEventListener('load', () => {
-    const preloader = document.querySelector('.preloader');
+    
     const animation = document.getElementById('animation1');
     //Disable Scroll 
     function disableScrolling () {
@@ -8,21 +10,21 @@ window.addEventListener('load', () => {
     }
 
     disableScrolling();
-    //Fade out function
-    function fadeOut() {
-        var initOpacity = 1;
-        var timer = setInterval(() => {
-            if(initOpacity <= 0.3){
-                clearInterval(timer);
-                preloader.style.display = "none";
-            }
+   // Fade out function
+    // function fadeOut() {
+    //     var initOpacity = 1;
+    //     var timer = setInterval(() => {
+    //         if(initOpacity <= 0.3){
+    //             clearInterval(timer);
+    //             preloader.style.display = "none";
+    //         }
 
-            preloader.style.opacity = initOpacity;
-            preloader.style.filter = 'alpha(opacity' + initOpacity * 100 + ")";
-            initOpacity -= initOpacity * 0.2;
+    //         preloader.style.opacity = initOpacity;
+    //         preloader.style.filter = 'alpha(opacity' + initOpacity * 100 + ")";
+    //         initOpacity -= initOpacity * 0.2;
             
-        }, 250);
-    }
+    //     }, 250);
+    // }
 
     setTimeout(() => {
         const frames = document.getElementById("animation1").children;
@@ -36,30 +38,9 @@ window.addEventListener('load', () => {
         //animation.remove();
     }, 600);
     setTimeout(() => {
-        //preloader.style.display = "none";
-        fadeOut();
+        //fadeOut();
         animation.remove();
         document.body.style.overflowY = "visible";
+        preloader.style.display = "none";
     }, 1800);
 }); 
-
-// window.addEventListener('load', () => {
-//     const preloader = document.querySelector('.preloader');
-//     const animation1 = document.getElementById('animation1');
-//     const frames = document.getElementById("animation1").children;
-//     console.log(frames.length);
-//     const sleep = (ms) => {
-//         return new Promise(resolve, ms);
-//     }
-
-//     const slowDown = async () => {
-//         for(let i = 0; i < frames.length ; i++){
-//             console.log(frames[i]);
-//                 await sleep(100)
-//                 frames[i].style.display = 'block';
-//                 frames[i].style.display = 'none';
-            
-//         }
-//     }
-    
-// });
--- template-parts/about-me.php
+++ template-parts/about-me.php
@@ -16,14 +16,15 @@
         <h3 id="skills">Skillset</h3>
         <article>
             <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/html-5.png">
-            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/css-3.png" alt="">
-            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/sass.png" alt="">
-            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/js.png" alt=""></i>
-            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/github.png" alt="">
-            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/bootstrap-1.png" alt="">
-            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/php.png" alt="">
-            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/figma.png" alt=""></i>
-            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/c-.png" alt="">
+            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/css-3.png" >
+            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/sass.png" >
+            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/js.png" >
+            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/github.png" >
+            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/bootstrap-1.png" >
+            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/php.png" >
+            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/figma.png" >
+            <img class="icons" src="http://miles-marie-angels.local/wp-content/uploads/2022/06/c-.png">
+            <img class="icons" src="https://miles-marie-angels.local/wp-content/uploads/2022/07/wordpress.png">
         </article>
     </div>
     <div class="background">
