b938d929bc96d5c7be4ae42d118644d9033cf368
MilesAngels <mangels@alphaworks.tech>
2022-07-28 15:39:43 -0700

    projects gallery

--- template-parts/projects-gallery.php
+++ template-parts/projects-gallery.php
@@ -3,48 +3,48 @@
     <div class="cards" style="background:url('<?php the_field('image_1'); ?>') center center; background-size: cover; background-repeat: no-repeat;">
         <div class="card-content">
             <h2 class="card-title"><?php the_field('website_name_1'); ?></h2>
-            <a href="https://milesangels.github.io/FinalProject/index.html">
-                <button class="redirect-button">Learn More!</button>
+            <a href="<?php the_field('url_1')?>" target="_blank">
+                <button class="redirect-button">View</button>
             </a>
         </div>
     </div>
     <div class="cards" style="background:url('<?php the_field('image_2'); ?>') center center; background-size: cover; background-repeat: no-repeat;">
         <div class="card-content">
             <h2 class="card-title"><?php the_field('website_name_2'); ?></h2>
-            <a href="https://milesangels.github.io/astronomy-exs/">
-                <button class="redirect-button">Learn More!</button>
+            <a href="<?php the_field('url_2')?>" target="_blank">
+                <button class="redirect-button">View</button>
             </a>
         </div>
     </div>
     <div class="cards" style="background:url('<?php the_field('image_3'); ?>') center center; background-size: cover; background-repeat: no-repeat;">
         <div class="card-content">
             <h2 class="card-title"><?php the_field('website_name_3'); ?></h2>
-            <a href="https://miles-marie-angels.local/404">
-                <button class="redirect-button">Learn More!</button>
+            <a href="<?php the_field('url_3')?>" target="_blank">
+                <button class="redirect-button">View</button>
             </a>
         </div>
     </div>
     <div class="cards" style="background:url('<?php the_field('image_4'); ?>') center center; background-size: cover; background-repeat: no-repeat;">
         <div class="card-content">
             <h2 class="card-title"><?php the_field('website_name_4'); ?></h2>
-            <a href="https://milesangels.github.io/Fancy-box/">
-                <button class="redirect-button">Learn More!</button>
+            <a href="<?php the_field('url_4')?>" target="_blank">
+                <button class="redirect-button">View</button>
             </a>
         </div>
     </div>
     <div class="cards" style="background:url('<?php the_field('image_5'); ?>') center center; background-size: cover; background-repeat: no-repeat;">
         <div class="card-content">
             <h2 class="card-title"><?php the_field('website_name_5'); ?></h2>
-            <a href="https://codepen.io/milesangels/pen/qBodzEP">
-                <button class="redirect-button">Learn More!</button>
+            <a href="<?php the_field('url_5')?>" target="_blank">
+                <button class="redirect-button">View</button>
             </a>
         </div>
     </div>
     <div class="cards" style="background:url('<?php the_field('image_6'); ?>') center center; background-size: cover; background-repeat: no-repeat;">
         <div class="card-content">
             <h2 class="card-title"><?php the_field('website_name_6'); ?></h2>
-            <a href="https://codepen.io/milesangels/pen/XWEmQrz?editors=1100">
-                <button class="redirect-button">Learn More!</button>
+            <a href="<?php the_field('url_6')?>" target="_blank">
+                <button class="redirect-button">View</button>
             </a>
         </div>
     </div>
